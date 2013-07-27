0844164ef380b4fb07ec03cd4b2f8e223849073a
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-07-27 13:43:12 +0900

    Initial Commit

---
+++ GestureFlashTests/GestureFlashTests.h
@@ -0,0 +1,13 @@
+//
+//  GestureFlashTests.h
+//  GestureFlashTests
+//
+//  Created by Takeshi Bingo on 2013/07/27.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GestureFlashTests : SenTestCase
+
+@end
---
+++ GestureFlashTests/GestureFlashTests.m
@@ -0,0 +1,32 @@
+//
+//  GestureFlashTests.m
+//  GestureFlashTests
+//
+//  Created by Takeshi Bingo on 2013/07/27.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import "GestureFlashTests.h"
+
+@implementation GestureFlashTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GestureFlashTests");
+}
+
+@end
